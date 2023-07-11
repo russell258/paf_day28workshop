51acb32441449b93de33cdd3453cc8270b33b5a1
russell258 <duckie258@gmail.com>
2023-07-11 14:37:30 +0800

    setup

---
+++ src/main/java/sg/edu/nus/iss/day28workshop/Day28workshopApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.day28workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day28workshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day28workshopApplication.class, args);
+	}
+
+}
