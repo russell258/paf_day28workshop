6706396416c5669979c97fe9e5f978a17698bc30
russell258 <duckie258@gmail.com>
2023-07-12 16:23:31 +0800

    mongo query repo

--- src/main/java/sg/edu/nus/iss/day28workshop/repository/PokemonRepository.java
+++ src/main/java/sg/edu/nus/iss/day28workshop/repository/PokemonRepository.java
@@ -2,8 +2,12 @@ package sg.edu.nus.iss.day28workshop.repository;
 
 import java.util.List;
 
+import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Repository;
 
@@ -20,6 +24,18 @@ public class PokemonRepository {
         return template.findDistinct(new Query(), "type", "pokemon", String.class);
     }
 
+    public List<Document> getPokemonByType(String type){
+        //create filter
+        Criteria criteria = Criteria.where("type").regex(type,"i");
 
+        //create query
+        Query query = Query.query(criteria)
+                            .with(Sort.by(Direction.ASC,"name"));
+
+        //perform projection
+        query.fields().exclude("_id").include("id","name","image","type","stats");
+        
+        return template.find(query,Document.class,"pokemon");
+    }
 
 }
